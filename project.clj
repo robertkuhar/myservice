c1b0b9f645099384f9e4c343228df27aca7e1871
robert.kuhar <robert.kuhar@climate.com>
2016-03-02 12:10:39 -0800

    Add some tests to the enpoints

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [compojure "1.4.0"]
+                 [metosin/compojure-api "1.0.1"]
                  [ring/ring-defaults "0.1.5"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
--- test/myservice/core_test.clj
+++ test/myservice/core_test.clj
@@ -1,7 +1,41 @@
 (ns myservice.core-test
   (:require [clojure.test :refer :all]
-            [myservice.core :refer :all]))
+            [myservice.core :as service]
+            [ring.adapter.jetty :as jetty]
+            [clojure.tools.logging :as log]
+            [clj-http.client :as client]))
+
+(defn make-service-url
+  [port path]
+  (let [service-url (format "http://localhost:%d/myservice/%s" port path)]
+    (log/info service-url)
+    service-url))
 
 (deftest a-test
   (testing "Placeholder"
     (is (= 1 1))))
+
+(deftest endpoints
+  (let [jetty-options {:port 0 :join? false}
+        server (jetty/run-jetty service/standalone-app jetty-options)
+        _ (service/init!)
+        _ (.start server)
+        port (.getLocalPort (first (.getConnectors server)))
+        make-url (partial make-service-url port)]
+
+    (testing "get healthz"
+      (let [response (client/get (make-url "healthz") {:throw-exceptions false})
+            {:keys [status body]} response]
+        (is (= status 200))))
+
+    (testing "get echo"
+      (let [query-params {"one" "Uno" "two" "Dos"}
+            response (client/get (make-url "echo") {:query-params query-params
+                                                    :throw-exceptions false})
+            {:keys [status body]} response]
+        (is (= status 200))
+        (doseq [x (flatten (seq query-params))]
+          (let [pattern (re-pattern x)]
+            (is (not (nil? (re-find pattern (:body response)))))))))
+
+    (.stop server)))
